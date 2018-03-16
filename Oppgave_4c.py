80f8ab481f415371126cbae7d133d9cda2a0a4bd
trulsmt <trulsmt@stud.ntnu.no>
2018-03-16 13:36:35 +0100

    ferdig med oppgave 4c

--- Oppgave_4c.py
+++ Oppgave_4c.py
@@ -1,29 +1,33 @@
 import numpy as np
-import scipy as sc
-from scipy.sparse import csr_matrix
 import oppgave_2
 
+storrelse_matrise = 10
 
 def y(x):
-    w, d = 0.3, 0.003
-    f = -480*w*d*9.81
-    E = 1.3*10**10
-    I = ((w*d)**3)/12
+    w, d = 0.3, 0.03
+    f = -480 * w * d * 9.81
+    E = 1.3 * 10**10
+    I = (w * d**3)/12
     L = 2.0
 
-    return (f/24*E*I)*x**2*(x**2-4*L*x+6*L**2)
+    return (f/(24*E*I)) * x**2 * (x**2 - 4*L*x + 6*L**2)
 
 
 def lag_ye():
     ye = []
     x = 0.2
-    for i in range(10):
+    for i in range(storrelse_matrise):
         ye.append(y(x))
         x += 0.2
-    return np.matrix(ye)
+    return ye
 
 
 ye = lag_ye()
-A = oppgave_2.lag_a(10)
-result = A*ye
+print("Ye")
+print(ye, "\n")
+A = oppgave_2.lag_a(storrelse_matrise)
+konst = 1/0.2**4
+a_1 = A * konst
+result = np.dot(a_1, ye)
+print("RESULTAT")
 print(result)
