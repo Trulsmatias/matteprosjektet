234eb89138948ba294e8ecbdc7344ea3cdcc6e4f
tonivuc <tonivuc@gmail.com>
2018-03-21 15:50:15 +0100

    Mulig denne ikke funker sånn kjempebra over alt. Men Kimia vil ha.

---
+++ oppgave_4d.py
@@ -0,0 +1,11 @@
+#Vi ønsker oss kondisjonstallet til A
+#Koden finnes i oppgave 5.
+
+import oppgave_5
+import oppgave_2
+
+n=10
+
+A = oppgave_2.lag_a(n)
+print(oppgave_5.cond(A))
+print(oppgave_5.condTungvint(A))
