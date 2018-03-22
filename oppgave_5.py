73bbd7b1f37612b4920d95fc683d9d687c1cb5d4
kimiaa <kimiaa@stud.ntnu.no>
2018-03-22 08:52:48 +0100

    Endrer oppgave_5 til å fungere etter at jeg gjorde noen endringer i går som gjorde at programmet ikke kjørte lengre.

--- oppgave_5.py
+++ oppgave_5.py
@@ -3,17 +3,19 @@ import oppgave_3
 import numpy
 from numpy.linalg import inv
 
+L = 2.0 # m
+w = 0.3  # m
+d = 0.03  # m
+E = 1.3 * 10**10
+I = (w * d**3) / 12
+f = -480 * w * d * 9.81  #  g = 9.81. Er konstant siden det er lik vekt over hele objektet
+
+
 def vektor():
-    w = 0.3  # m
-    d = 0.03  # m
-    E = 1.3 * 10**10
-    I = (w * d**3) / 12
-    f = -480 * w * d * 9.81  #  g = 9.81. Er konstant siden det er lik vekt over hele objektet
     return f/(E*I)
 
+
 print("vektoren: ", vektor()) # Får å få vektoren f/EI (1, 1, ... ,1)
-
-
 
 
 def y(x):
@@ -23,14 +25,15 @@ def y(x):
 def cond(matrise):  # har bestemt at skal bruke 1, altså summen av kolonnen som gir størst sum når summert
     return numpy.linalg.cond(matrise, numpy.inf)
 
+
 def condTungvint(matrise):
     matriseInv = inv(matrise)
     return numpy.dot(numpy.linalg.norm(matrise,numpy.inf),numpy.linalg.norm(matriseInv,numpy.inf))
 
+
 def tabell_over_feil_i_punktet():
     n = 20 #Funker ikke under 6
     noyaktig = y(2)
-
     while n <= 10*2**11:
         if n < 10 * 2**10:  # dette er bare for at programmt ikke skal kjøre i en evighet
             print("kondisjonstall", cond(oppgave_2.lag_a(n)))
@@ -40,4 +43,4 @@ def tabell_over_feil_i_punktet():
         n *= 2
 
 
-#tabell_over_feil_i_punktet()
+tabell_over_feil_i_punktet()
